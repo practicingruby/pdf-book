983ed2cd5c0b549a4cc4575dfcb41286ff7ed9d5
Gregory Brown <gregory.t.brown@gmail.com>
2014-08-13 11:31:13 -0400

    Add component idea

--- examples/utility_bill/components/customer_address.rb
+++ examples/utility_bill/components/customer_address.rb
@@ -1,3 +1,15 @@
+# Alternative interface
+#
+# Prawn::Component.define(:customer_address) do |params|
+#   text params[:address], :size => 10
+# end
+#
+# then elsewhere...
+#
+# draw(:customer_address, :address => "...")
+#
+# Would create an anonymous class under the hood.
+
 require_relative "../helper"
 
 module UtilityBill
